69c6469ce068b684fa34d60d2ebef8744855c3ab
pawut jingjit <pawut.j@gmail.com>
2025-08-10 04:27:20 +0700

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-
+import vercel from '@astrojs/vercel/static';
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
